8803172896accae9693b13c583019b5e3394a945
JAGUIN Glen <g.jaguin@highco-data.fr>
2013-04-05 17:23:44 +0200

    Rename insert command and close window after copy

--- PhpNamespaceCopy.py
+++ PhpNamespaceCopy.py
@@ -11,14 +11,13 @@ def get_namespace(window):
     rg = window.active_view().find('(class|interface) [a-zA-Z]+', 0)
     sclass = window.active_view().substr(rg)
     sclass = sclass[(sclass.index(' ')+1):]
-    return snamespace+'\\'+sclass+';'
+    return snamespace+'\\'+sclass
 
 def insert_use_statement(window, namespace):
-    instruct = 'use '+namespace
+    instruct = 'use '+namespace+';'
     sublime.set_clipboard(instruct)
     window.run_command('close_file', [])
     window.run_command('hide_overlay', [])
-    window.focus_view(window.active_view())
     if is_php_file(window):
         edit = window.active_view().begin_edit()
         regions = window.active_view().find_all('use (.*);', 0)
@@ -36,8 +35,11 @@ class PhpNamespaceCopyCommand(sublime_plugin.WindowCommand):
     def run(self):
         if is_php_file(self.window):
             sublime.set_clipboard(get_namespace(self.window))
+            self.window.run_command('close_file', [])
+            self.window.run_command('hide_overlay', [])
 
-class PhpInsertUseCommand(sublime_plugin.WindowCommand):
+class PhpNamespaceInsertUseCommand(sublime_plugin.WindowCommand):
     def run(self):
+        print "test"
         if is_php_file(self.window):
             insert_use_statement(self.window, get_namespace(self.window))
